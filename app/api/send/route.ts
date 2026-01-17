6719bd47f560c1de1b694ff3da84fe15936fd77b
Federico <federicoaniello@hotmail.it>
2026-01-17 19:01:44 +0100

    email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,14 +1,15 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+  const emailT = process.env.EMAIL;
+  if(!emailT) throw new Error('No email from ENV!');
   try {
     const body = await req.json();
     const { name, email, message } = body;
     const data = await resend.emails.send({
       from: 'email <onboarding@resend.dev>',
-      to: ['federicoaniello@hotmail.it'],
+      to: [emailT],
       subject: 'New message from your portfolio',
       text: `<div>
     <h1>New message from ${name}!</h1>
